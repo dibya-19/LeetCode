cdb1b14b626e8759ebdd4f482737b45a12bc986d
Dibyalochan Swain <dibyalochanswain123@gmail.com>
2026-07-11 20:26:01 +0530

    Time: 0 ms (100%), Space: 13 MB (29.21%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,15 +1,21 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        n = len(nums)
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         low = 0
-        high = n-1
-        ans = n
-        while(low<=high):
-            mid = (low+high)//2
-            if (nums[mid] >= target):
-                ans = mid
-                high = mid - 1
-            else:
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low+high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
                 low = mid + 1
-        return ans
+
+            else:
+                high = mid - 1
+
+        return low
         
