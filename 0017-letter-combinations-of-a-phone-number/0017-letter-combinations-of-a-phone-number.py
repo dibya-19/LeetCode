3290192afe65cfe91c91d4647da655bff79a0ae1
Dibyalochan Swain <dibyalochanswain123@gmail.com>
2026-07-20 17:11:02 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.73%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def solve(self, index, subset, digits, result):
+        char_map = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+
+        if index == len(digits):
+            result.append("".join(subset))
+            return
+
+        for ch in char_map[digits[index]]:
+            subset.append(ch)
+            self.solve(index + 1, subset, digits, result)
+            subset.pop()
+
+    def letterCombinations(self, digits):
+
+        if not digits:
+            return []
+
+        result = []
+        self.solve(0, [], digits, result)
+
+        return result
